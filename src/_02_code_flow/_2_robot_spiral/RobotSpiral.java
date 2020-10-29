9cad874b7a4c86b346d2c42d056d343a9ccd7ea6
root <root@45de5ba9cb26>
2020-10-29 22:27:02 +0000

    Finish Flaming Ninja Star

--- src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
+++ src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
@@ -24,23 +24,23 @@ public class RobotSpiral {
 		// LOOP. Start a while loop to repeat the COLOR, DRAW, TURN, and COUNT code below until 50 lines have been drawn
 
   	   while ( count < 50 ) { 
-		setRandomPenColor();
-  		   rob.move(50);
+		rob.setRandomPenColor();
+  		   rob.move(5*count);
   	       rob.turn(360/7);
-  	     setPenWidth(100);
+  	     rob.setPenWidth(count);
   	       count+=1;
   	   }
 			// COLOR.   Have the robot set a random pen color:      setRandomPenColor()
-                setRandomPenColor();
+                rob.setRandomPenColor();
 			// DRAW.    Move the robot (5*count) pixels
 			//          count is the name of the variable you created earlier
-                rob.move(50);
+                rob.move(5*count);
 			// TURN.    Turn the robot (360/7) degrees to the right
                 rob.turn(360/7); 
 			// COUNT.   Increase the count of how many lines have been drawn so far ( count+=1 )
 
 			// Change the robot pen width to the current value of the count variable 
-                setPenWidth(50);
+                rob.setPenWidth(count);
 
 		//  End the while loop here
 
@@ -50,15 +50,7 @@ public class RobotSpiral {
 
 		
 	}
-
-	private static void setPenWidth(int i) {
-		// TODO Auto-generated method stub
 		
 	}
 
-	private static void setRandomPenColor() {
-		// TODO Auto-generated method stub
-		
-	}
-}
 
